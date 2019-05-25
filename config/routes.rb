3cc0b5cbfbe7b23f46f57be736f00c21d2033056
Oleg Sokolov <olegsokolov91@gmail.com>
2019-05-25 16:38:23 +0200

    add redirect after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root 'pages#home'
   get 'welcome', to: 'pages#home'
   get 'products', to: 'products#index'
 end
