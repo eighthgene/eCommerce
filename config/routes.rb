3d69884747d6efb0fc378207e492656e26460b63
Oleg Sokolov <olegsokolov91@gmail.com>
2019-05-25 21:43:31 +0200

    redesign nav bar and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#home'
+  root 'pages#index'
   get 'welcome', to: 'pages#index'
   get 'products', to: 'products#index'
 end
