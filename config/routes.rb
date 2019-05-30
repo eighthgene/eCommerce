f57c99ea6d24fd7a08506fc9a251c6507f982bee
Oleg Sokolov <olegsokolov91@gmail.com>
2019-05-30 21:53:40 +0200

    redesign and make responsive nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
       registrations: 'registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'products#index'
   get 'welcome', to: 'pages#index'
   get 'products', to: 'products#index'
 end
