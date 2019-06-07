7123e5f07f9f027880f2b3ed0a7d35fc8c36434d
Oleg Sokolov <olegsokolov91@gmail.com>
2019-06-08 00:39:44 +0200

    finished cart (orders + order_items), left to implement payment (PayPal)

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -19,6 +19,7 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
+    redirect_back fallback_location: root_path
   end
 
 
